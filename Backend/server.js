3d42a1791d6a83e61cb4b3f7d89769bfa989d8bf
Muhammed Bilal <muhammedbilal6211@gmail.com>
2025-07-22 17:32:04 +0530

    usermodel and usercontroller structure

--- Backend/server.js
+++ Backend/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./config/db.js";
+import userRoute from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-connectDb()
+connectDb();
 
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-app.use(cors())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const port = process.env.PORT;
+
+app.use("/api/user", userRoute);
 
 app.listen(port, () => console.log("server started successfully"));
