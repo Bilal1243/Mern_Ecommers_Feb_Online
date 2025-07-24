718658733e0cded0764eba0ed4b32714c0092c7b
Muhammed Bilal <muhammedbilal6211@gmail.com>
2025-07-24 17:37:38 +0530

    completed middlewares and user basic controllers

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,15 +1,63 @@
 import Users from "../models/userModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
 
-const registerUser = async () => {};
+const registerUser = asyncHandler(async (req, res) => {
+  let { name, email, password } = req.body;
 
+  const salt = await bcrypt.genSalt(10);
+  const encryptedPassword = await bcrypt.hash(password, salt);
 
-const loginUser = async () => {};
+  const userExists = await Users.findOne({ email: email });
 
+  if (userExists) {
+    return res.status(400).json({ message: "user already exists" });
+  }
 
-const logoutUser = async () => {};
+  const user = await Users.create({
+    name,
+    email,
+    password: encryptedPassword,
+  });
 
-export {
-    registerUser,
-    loginUser,
-    logoutUser
-}
+  if (user) {
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    return res.status(400).json({ message: "Invalid user data" });
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  let { email, password } = req.body;
+
+  const user = await Users.findOne({ email: email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    return res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    return res.status(400).json({ message: "invaid user data" });
+  }
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expiresIn: new Date(0),
+  });
+
+  res.status(200).json({ message: "logout success" });
+});
+
+export { registerUser, loginUser, logoutUser };
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./config/db.js";
 import userRoute from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middlewares/errorMiddlewares.js";
 
 dotenv.config();
 
@@ -12,10 +14,14 @@ connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors());
 
 const port = process.env.PORT;
 
 app.use("/api/user", userRoute);
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log("server started successfully"));
