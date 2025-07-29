f936b99113295d4b6f0a0c90d47a76680d94ad28
Muhammed Bilal <muhammedbilal6211@gmail.com>
2025-07-29 17:36:05 +0530

    completed login and register ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen/>}/>
+        <Route path="/register" element={<RegisterScreen/>}/>
       </Routes>
     </>
   );
