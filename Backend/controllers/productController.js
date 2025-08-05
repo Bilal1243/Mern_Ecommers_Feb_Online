c6dda636f6903db64d692541edbce48835601250
Muhammed Bilal <muhammedbilal6211@gmail.com>
2025-08-05 17:50:10 +0530

    completed product controller and product apislice

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -4,13 +4,75 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 
 const getProducts = asyncHandler(async (req, res) => {});
 
-const getProductById = asyncHandler(async (req, res) => {});
+const getProductById = asyncHandler(async (req, res) => {
+  let product = await Products.findById(req.params.id);
 
-const createProduct = asyncHandler(async (req, res) => {});
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product Not Found");
+  }
+});
 
-const updateProduct = asyncHandler(async (req, res) => {});
+const createProduct = asyncHandler(async (req, res) => {
+  const { name, brand, category, description, price, countInStock } = req.body;
 
-const deleteProduct = asyncHandler(async (req, res) => {});
+  const image = req.file ? req.file.path : null;
+
+  const product = await Products.create({
+    user: req.user._id,
+    name,
+    brand,
+    category,
+    description,
+    price,
+    countInStock,
+    image,
+  });
+
+  if (product) {
+    return res.status(201).json(product);
+  }
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  let { name, price, category, countInStock, brand, description } = req.body;
+
+  let product = await Products.findById(req.params.id);
+
+  if (product) {
+
+    product.name = name || product.name;
+    product.price = price || product.price;
+    product.category = category || product.category;
+    product.countInStock = countInStock || product.countInStock;
+    product.brand = brand || product.brand;
+    product.description = description || product.description;
+    product.image = req.file ? req.file.path : product.image;
+
+    const productUpdate = await product.save()
+
+    res.json(productUpdate)
+
+  }else{
+    req.status(404)
+    throw new Error('Product Not Found')
+  }
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  let product = await Products.findById(req.params.id)
+
+  if(product){
+    await Products.deleteOne({_id : product._id})
+    res.json({message : 'product removed'})
+  }else{
+    res.status(404)
+    throw new Error('Product Not Found')
+  }
+
+});
 
 export {
   getProducts,
