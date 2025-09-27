236fca08fd4bed294f22630c7515b47faf4f50a1
Muhammed Bilal <muhammedbilal6211@gmail.com>
2025-09-27 17:09:03 +0530

    changed frontend for hosting

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://mern-ecommers-bacend.onrender.com",
+  }),
   tagTypes: ["User", "Users", "Products", "Product"],
   endpoints: () => ({}),
 });
